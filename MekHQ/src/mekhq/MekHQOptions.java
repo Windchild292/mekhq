2d7a68593e1ffad3a826d973a3fa0ab5b437454d
Windchild292 <jrbowen24@gmail.com>
2020-09-15 20:01:51 -0600

    Adding MekHQ configuable option to add the MRMS to the command center

--- MekHQ/src/mekhq/MekHQOptions.java
+++ MekHQ/src/mekhq/MekHQOptions.java
@@ -18,6 +18,9 @@
  */
 package mekhq;
 
+import mekhq.campaign.event.MekHQOptionsChangedEvent;
+import mekhq.campaign.event.PersonChangedEvent;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.prefs.Preferences;
@@ -61,6 +64,15 @@ public final class MekHQOptions {
 
     public void setCommandCenterUseUnitMarket(boolean value) {
         userPreferences.node(MekHqConstants.DISPLAY_NODE).putBoolean(MekHqConstants.COMMAND_CENTER_USE_UNIT_MARKET, value);
+    }
+
+    public boolean getCommandCenterMRMS() {
+        return userPreferences.node(MekHqConstants.DISPLAY_NODE).getBoolean(MekHqConstants.COMMAND_CENTER_MRMS, false);
+    }
+
+    public void setCommandCenterMRMS(boolean value) {
+        userPreferences.node(MekHqConstants.DISPLAY_NODE).putBoolean(MekHqConstants.COMMAND_CENTER_MRMS, value);
+        MekHQ.triggerEvent(new MekHQOptionsChangedEvent());
     }
     //endregion Command Center Display
     //endregion Display
