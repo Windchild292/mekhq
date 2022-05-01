d3cfa63c981718cff8230f4f160b841a8dce1469
unknown <jrbowen24@gmail.com>
2022-05-01 13:21:45 -0400

    Adding Personnel Reporting

---
+++ MekHQ/src/mekhq/campaign/report/ProcreationReport.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022 - The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ. If not, see <http://www.gnu.org/licenses/>.
+ */
+package mekhq.campaign.report;
+
+/**
+ * I wonder what values for number of personnel and pregnancy chances in the campaign options would
+ * create a self-sustaining mercenary company
+ *
+ * This report displays the , assuming regular aging with a death rate of zero.
+ * @author Justin "Windchild" Bowen
+ */
+public class ProcreationReport extends AbstractReport {
+}
